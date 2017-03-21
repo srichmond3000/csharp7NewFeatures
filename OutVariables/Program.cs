4f06ea4fe33c8e943276efa79d8ec37e23d9372d
srichmond3000 <srichmond3000@googlemail.com>
2017-03-21 21:32:32 +0000

    Added Out Variables project.

---
+++ OutVariables/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SetValues(out string name, out int index);
+
+            Console.WriteLine($"Name: {name}, Index: {index}");
+            Console.ReadLine();
+        }
+
+        private static void SetValues(out string name, out int index)
+        {
+            name = "Fred";
+            index = 3;
+        }
+    }
+}
