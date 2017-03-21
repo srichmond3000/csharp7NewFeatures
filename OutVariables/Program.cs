66d304c9bf90626744fb1b659a63f91ee05562f5
srichmond3000 <srichmond3000@googlemail.com>
2017-03-21 21:39:39 +0000

    Added more uses of out variables.

--- OutVariables/Program.cs
+++ OutVariables/Program.cs
@@ -13,6 +13,23 @@ namespace OutVariables
             SetValues(out string name, out int index);
 
             Console.WriteLine($"Name: {name}, Index: {index}");
+
+            // also use of _ for 'discards', i.e. parameters we don't care about.
+            SetValues(out string anotherName, out _);
+
+            Console.WriteLine($"Name: {anotherName}");
+
+            // can use 'var' as compiler can infer types.
+            SetValues(out var inferredName, out var inferredIndex);
+
+            Console.WriteLine($"Inferred name: {inferredName}. Inferred index: {inferredIndex}");
+
+            // use with 'TryParse' pattern.
+            if(Int32.TryParse("2", out var result))
+            {
+                Console.WriteLine($"Result = {result}");
+            }
+
             Console.ReadLine();
         }
 
